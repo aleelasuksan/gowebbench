e7dc499120d52c24bc3371db14b476c9808aaad5
Atit Leelasuksan <grief.d.lament@gmail.com>
2015-09-08 13:50:33 +0700

    webcrawler work in asynchronous instead of sequential and both webcrawler and loadtest write result to log file (still use static path)

--- loadtest.go
+++ loadtest.go
@@ -9,16 +9,17 @@ import "time"
 // import "runtime/debug"
 import "runtime"
 import "io/ioutil"
-// import "sync"
 import "log"
-// import "net"
 import "regexp"
+import "bytes"
 
 type Response_Stat struct {
   status string
   response_time time.Duration
   amount_of_data int
 }
+
+var f *os.File
 
 func main() {
   flag.Parse()
@@ -48,23 +49,26 @@ func main() {
     os.Exit(1)
   }
 
-  // var wg sync.WaitGroup
-  // wg.Add(user)
+  filename := "D:\\src\\loadtest.log"
+  f, err = os.OpenFile(filename, os.O_WRONLY | os.O_APPEND | os.O_CREATE, 0666)
+  if err != nil {
+    log.Printf("%T %+v\n", err, err)
+  }
 
   result := make(chan Response_Stat, user)
   transport := http.Transport{
     DisableKeepAlives: false,
     MaxIdleConnsPerHost: user,
-    // ResponseHeaderTimeout: 60 * time.Second,
+    ResponseHeaderTimeout: 60 * time.Second,
     }
 
   start := time.Now()
   for i := 0 ; i < user ; i++ {
     go sendRequest(uri, trans, result, &transport)
-    // time.Sleep(100 * time.Millisecond)
   }
+
   fmt.Printf("Start test %s...\n", uri)
-  // wg.Wait()
+  writeLog(fmt.Sprintf("Start test %s...\r\n", uri))
 
   count := 0
   success := 0
@@ -79,10 +83,14 @@ func main() {
   for ; count != user * trans && !timeout ; {
     select {
     case s := <-result:
-      fmt.Printf("%4d : Status:%s ,Response time:%.4fsec ,Bytes:%v\n", count, s.status, s.response_time.Seconds(), s.amount_of_data)
+
+      fmt.Printf("%6d : Status:%s ,Response time:%.4fsec ,Bytes:%v\n", count, s.status, s.response_time.Seconds(), s.amount_of_data)
+      writeLog(fmt.Sprintf("%6d : Status:%s ,Response time:%.4fsec ,Bytes:%v\n", count, s.status, s.response_time.Seconds(), s.amount_of_data))
+
       r, err := regexp.Compile("100|101|102|200|201|202|203|204|205|206|207|208|226|300|301|302|303|304|305|306|307|308")
       if err != nil {
         log.Printf("%T %+v\n", err, err)
+        writeLog(fmt.Sprintf("%T %+v\r\n", err, err))
       } else {
         if r.MatchString(s.status) == true {
           if(s.response_time.Seconds() > max_res) {
@@ -99,25 +107,46 @@ func main() {
         }
       }
       count++
-    // case <-time.After(5 * time.Second):
-    //   fmt.Println("TIMEOUT!")
-    //   timeout = true
     }
   }
   end := time.Now()
   close(result)
+
   fmt.Println("=============== SUMMARY ================")
+  writeLog("=============== SUMMARY ================\r\n")
+
   fmt.Println("Target address:", uri)
+  writeLog(fmt.Sprintf("Target address: %v\r\n", uri))
+
   fmt.Println("Concurrent users:", user)
+  writeLog(fmt.Sprintf("Concurrent users: %v \r\n", user))
+
   fmt.Println("Total transaction:", user * trans)
+  writeLog(fmt.Sprintf("Total transaction: %v\r\n", user * trans))
+
   fmt.Println("Elapsed time:", end.Sub( start ))
+  writeLog(fmt.Sprintf("Elapsed time: %v\r\n", end.Sub( start )))
+
   fmt.Println("Successful transaction:", success)
+  writeLog(fmt.Sprintf("Successful transaction: %v\r\n", success))
+
   fmt.Println("Failed transaction:", ( user * trans ) - success)
+  writeLog(fmt.Sprintf("Failed transaction: %v\r\n", ( user * trans ) - success))
+
   fmt.Println("Total response data:", total_data)
+  writeLog(fmt.Sprintf("Total response data: %v \r\n", total_data))
+
   fmt.Println("Transaction rate:", float64( count ) / end.Sub( start ).Seconds(), "trans/sec")
+  writeLog(fmt.Sprintf("Transaction rate: %v trans/sec\r\n", float64( count ) / end.Sub( start ).Seconds()))
+
   fmt.Println("Maximum response time:", max_res, "s")
+  writeLog(fmt.Sprintf("Maximum response time: %v sec\r\n", max_res))
+
   fmt.Println("Minimum response time:", min_res, "s")
+  writeLog(fmt.Sprintf("Minimum response time: %v sec\r\n", min_res))
+
   fmt.Println("Average response time:", sum_res / float64(success), "s")
+  writeLog(fmt.Sprintf("Average response time: %v sec\r\n", sum_res / float64(success)))
 }
 
 func sendRequest(uri string, n int, result chan Response_Stat, transport *http.Transport) {
@@ -125,20 +154,22 @@ func sendRequest(uri string, n int, result chan Response_Stat, transport *http.T
     req, err := http.NewRequest( "GET", uri, nil )
     if err != nil {
       fmt.Println("Panic request")
+      writeLog("Panic request")
       log.Printf("%T %+v\n", err, err)
+      writeLog(fmt.Sprintf("%T %+v\r\n", err, err))
       result <- Response_Stat{ "Error from Request", 0, 0 }
     } else {
-      // req.Close = false
       start := time.Now()
       res, err := transport.RoundTrip(req)
       end := time.Now()
       response_time := end.Sub(start)
       if err != nil {
         fmt.Println("Panic response")
+        writeLog("Panic response")
         log.Printf("%T %+v\n", err, err)
+        writeLog(fmt.Sprintf("%T %+v\r\n", err, err))
         result <- Response_Stat{ "Error from Response", response_time, 0 }
       } else {
-        //result <- fmt.Sprintf("%s %v", res.Status, response_time)
         l := int(res.ContentLength)
         result <- Response_Stat{res.Status, response_time, l}
         ioutil.ReadAll(res.Body)
@@ -146,5 +177,16 @@ func sendRequest(uri string, n int, result chan Response_Stat, transport *http.T
       }
     }
   }
-  // wg.Donw()
+}
+
+func writeLog(message string) {
+  var b bytes.Buffer
+  _, err := fmt.Fprintf(&b, message)
+  if err != nil {
+    return
+  }
+  _, err = f.Write(b.Bytes())
+  if err != nil {
+    return
+  }
 }
--- webcrawler.go
+++ webcrawler.go
@@ -10,14 +10,16 @@ import "flag"
 import "os"
 import "strconv"
 import "regexp"
+import "log"
 import "os/exec"
-
-type CrawlInfo struct {
-  URI string
-  Depth int
-}
+import "sync"
+import "bytes"
 
 var visited = make(map[string]bool)
+
+var wg sync.WaitGroup
+
+var f *os.File
 
 func main() {
   flag.Parse()
@@ -36,47 +38,79 @@ func main() {
 
   if err != nil {
     fmt.Println("Second argument is not a number.")
-    panic(err)
+    log.Printf("%T %+v\n", err, err)
     os.Exit(1)
   }
 
-  //var wg sync.WaitGroup
-  //queue := make(chan CrawlInfo)
-  //in := CrawlInfo{uri, depth}
-  //go func() { queue <- in }()
+  if depth < 1 {
+    fmt.Println("Depth is less than 1, Please specify depth equals 1 or greater.")
+    os.Exit(1)
+  }
 
-  //for uri := range queue {
-    fetchURI(uri, depth)
-    //wg.Add(1)
-    //go fetchURI(uri, queue, wg)
-  //}
-  fmt.Println("Done!")
-  usr := 10
-  trans := 5
-  path := "D:\\src\\loadtest.go"
-  for key, _ := range visited {
-    fmt.Println()
-    cmd := exec.Command("cmd", fmt.Sprintf("/C go run %s %s %d %d", path, key, usr, trans))
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.Run()
-    // output, err := cmd.Output()
-    // if err != nil {
-    //   fmt.Println("Exec commandline")
-    //   panic(err)
-    //   return
-    // }
-    // if len(output) > 0 {
-    //   fmt.Println(string(output))
-    // }
+  filename := "D:\\src\\crawling.log"
+  f, err = os.OpenFile(filename, os.O_WRONLY | os.O_APPEND | os.O_CREATE, 0666)
+  if err != nil {
+    log.Printf("%T %+v\n", err, err)
+  }
+
+  wg.Add(1)
+  go fetchURI(uri, depth)
+
+  wg.Wait()
+  writeLog("Done Crawling!\r\n\r\n")
+  fmt.Println("Done Crawling!\n")
+  f.Close()
+  if len(args) > 2 {
+    if args[2] == "-load" {
+      if len(args) < 4 {
+        fmt.Println("No amount of concurrent user given.")
+        os.Exit(1)
+      } else if len(args) < 5 {
+        fmt.Println("No amount of transaction per user given.")
+        os.Exit(1)
+      }
+
+      fmt.Println("Start Load Testing...")
+
+      usr, err := strconv.Atoi(args[3])
+      if err != nil {
+        fmt.Println("Fourth argument is not a number.")
+        log.Printf("%T %+v\n", err, err)
+        os.Exit(1)
+      }
+      trans, err := strconv.Atoi(args[4])
+      if err != nil {
+        fmt.Println("Fifth argument is not a number.")
+        log.Printf("%T %+v\n", err, err)
+        os.Exit(1)
+      }
+
+      path := "D:\\src\\WTesting\\loadtest.go"
+      for key, _ := range visited {
+        fmt.Println()
+        cmd := exec.Command("cmd", fmt.Sprintf("/C go run %s %s %d %d", path, key, usr, trans))
+        cmd.Stdout = os.Stdout
+        cmd.Stderr = os.Stderr
+        cmd.Run()
+      }
+
+      fmt.Println("Done Load Testing!")
+    }
   }
 }
 
 func fetchURI(uri string, depth int) {
-  if depth == 0 || visited[uri] {
+  fmt.Println("fetching: ", uri, depth)
+  writeLog(fmt.Sprintf("fetching: %v %v\r\n", uri, depth))
+  f.Sync()
+  if visited[uri] {
+    wg.Done()
+    return
+  } else if depth == 1 {
+    visited[uri] = true
+    wg.Done()
     return
   }
-  fmt.Println("fetching: ", uri , depth)
   visited[uri] = true
   transport := &http.Transport{
     TLSClientConfig: &tls.Config{
@@ -87,12 +121,12 @@ func fetchURI(uri string, depth int) {
   res, err := client.Get(uri)
   if err != nil {
     fmt.Println("Panic Get")
-    panic(err)
+    writeLog("Panic Get")
+    log.Printf("%T %+v\n", err, err)
+    writeLog(fmt.Sprintf("%T %+v\r\n", err, err))
     return
   }
   defer res.Body.Close()
-  // fmt.Println(res.Header["Content-Type"])
-  // fmt.Println(res.Header["Content-Length"])
   r, _ := regexp.Compile("html")
   if !r.MatchString(res.Header["Content-Type"][0]) {
     fmt.Println("Content-Type is not html.")
@@ -103,11 +137,12 @@ func fetchURI(uri string, depth int) {
     absolutePath := normalizeURL(link, uri)
     if uri != "" {
       if !visited[absolutePath] {
-        fetchURI(absolutePath, depth-1)
-        //go func() { queue <- CrawlInfo{absolutePath, info.Depth} }()
+        wg.Add(1)
+        go fetchURI(absolutePath, depth-1)
       }
     }
   }
+  wg.Done()
 }
 
 func fetchHyperLink(httpBody io.Reader) []string {
@@ -142,4 +177,16 @@ func normalizeURL(href, base string) string {
   }
   uri = baseURL.ResolveReference(uri)
   return uri.String()
+}
+
+func writeLog(message string) {
+  var b bytes.Buffer
+  _, err := fmt.Fprintf(&b, message)
+  if err != nil {
+    return
+  }
+  _, err = f.Write(b.Bytes())
+  if err != nil {
+    return
+  }
 }
